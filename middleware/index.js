83a8cad2515af0efc75a60fe53ab44e37e9cd6b4
johnbean36 <jmbass36@gmail.com>
2024-05-15 09:20:56 -0500

    bugs

--- middleware/index.js
+++ middleware/index.js
@@ -20,11 +20,15 @@ function createToken(payload){
 
 function stripToken(req, res, next){
     try{
+        console.log(req.headers);
         const token = req.headers['authorization'].split(' ')[1];
+        console.log(token);
         if(token){
+            console.log("condition passed")
             res.locals.token = token;
             return next();
         }
+        console.log("Oops something must be wrong;")
         res.status(401).send({status: 'Error', msg: 'Unauthorized'})
     }catch(err){
         console.log(err);
@@ -42,13 +46,16 @@ function verifyRights(req, res, next){
 }
 
 function verifyToken(req, res, next){
+    console.log(res.locals.token, "Verify token")
     const token = res.locals.token;
     try{
         let payload = jwt.verify(token, APP_SECRET);
+        console.log(payload, "payload")
         if(payload){
             res.local.payload = payload;
             return next()
         }
+        console.log("The error is here")
         res.status(401).send({status: 'Error', msg: 'Unauthorized'})
     }catch(error){
         console.log(error)
